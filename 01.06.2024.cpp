d2d4dd0a948db76d0ec1322f6fd1c191118a7651
Deepti Dwivedi <112163078+0221deepti@users.noreply.github.com>
2024-06-01 23:33:25 +0530

    Create 01.06.2024.cpp

---
+++ 01.06.2024.cpp
@@ -0,0 +1,13 @@
+problem-->You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+Return the score of s.
+**********************SOLUTION-->************************
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score=0;
+    for(int i=0;i<s.length()-1;i++){
+        score +=abs(s[i]-s[i+1]);
+    }
+    return score;
+    }
+};
